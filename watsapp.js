a5d2c6fd8829e5ed713649e9837af8ac8a8016ac
anshika1012jaiswal <anshika1012jaiswal@gmail.com>
2021-06-25 14:30:47 +0530

    initial commit

---
+++ watsapp.js
@@ -0,0 +1,242 @@
+var no1 = document.querySelector('#no1');
+no1.addEventListener('click', function() {
+
+    var name = document.getElementById('1');
+    name.innerHTML = "shagun";
+
+    var m1 = document.getElementById('3');
+    m1.innerHTML = "hiiii";
+
+    var m1 = document.getElementById('4');
+    m1.innerHTML = "hello anshika";
+
+    var m1 = document.getElementById('5');
+    m1.innerHTML = "bihsdaiuhcihsi";
+
+    var m1 = document.getElementById('6');
+    m1.innerHTML = "rduhjnkjl";
+
+    var m1 = document.getElementById('7');
+    m1.innerHTML = "hajsljlal";
+
+    var m1 = document.getElementById('8');
+    m1.innerHTML = "What's going on??? 🖖🖖";
+
+})
+
+
+var no2 = document.querySelector('#no2');
+no2.addEventListener('click', function() {
+
+    var name = document.getElementById('1');
+    name.innerHTML = "anshika";
+    var m1 = document.getElementById('2');
+    m1.innerHTML = "offline";
+
+    var m1 = document.getElementById('3');
+    m1.innerHTML = "🤣🤣v🤣";
+
+    var m1 = document.getElementById('4');
+    m1.innerHTML = "BJKSDNCKNAS";
+
+    var m1 = document.getElementById('5');
+    m1.innerHTML = "bihBSAJKNKLAS";
+
+    var m1 = document.getElementById('6');
+    m1.innerHTML = "rHIOKINSLAKCjl";
+
+    var m1 = document.getElementById('7');
+    m1.innerHTML = "SNIAONSAMOPAlal";
+
+    var m1 = document.getElementById('8');
+    m1.innerHTML = "SDMKpoks";
+
+})
+var no3 = document.querySelector('#no3');
+no3.addEventListener('click', function() {
+
+    var name = document.getElementById('1');
+    name.innerHTML = "yash";
+    var m1 = document.getElementById('2');
+    m1.innerHTML = "offline";
+
+    var m1 = document.getElementById('3');
+    m1.innerHTML = "🖖🖖🖖🖖🖖";
+
+    var m1 = document.getElementById('4');
+    m1.innerHTML = "YOYO";
+
+    var m1 = document.getElementById('5');
+    m1.innerHTML = "HONEY SINGH";
+
+    var m1 = document.getElementById('6');
+    m1.innerHTML = "FXHFVJ";
+
+    var m1 = document.getElementById('7');
+    m1.innerHTML = "hajsljlalBIHOIJOLJMOLJL UGHEW BJBNC IOEN";
+
+    var m1 = document.getElementById('8');
+    m1.innerHTML = "dsjojpoks";
+
+})
+var no3 = document.querySelector('#no3');
+no3.addEventListener('click', function() {
+
+    var name = document.getElementById('1');
+    name.innerHTML = "devanshi";
+    var m1 = document.getElementById('2');
+    m1.innerHTML = "offline";
+
+    var m1 = document.getElementById('3');
+    m1.innerHTML = "hiiii";
+
+    var m1 = document.getElementById('4');
+    m1.innerHTML = "hello anshika";
+
+    var m1 = document.getElementById('5');
+    m1.innerHTML = "bihsdaiuhcihsi";
+
+    var m1 = document.getElementById('6');
+    m1.innerHTML = "rduhjnkjl";
+
+    var m1 = document.getElementById('7');
+    m1.innerHTML = "hajsljlal";
+
+    var m1 = document.getElementById('8');
+    m1.innerHTML = "dsjojpoks";
+
+})
+var no3 = document.querySelector('#no3');
+no3.addEventListener('click', function() {
+
+    var name = document.getElementById('1');
+    name.innerHTML = "yash";
+    var m1 = document.getElementById('2');
+    m1.innerHTML = "offline";
+
+    var m1 = document.getElementById('3');
+    m1.innerHTML = "hiiii";
+
+    var m1 = document.getElementById('4');
+    m1.innerHTML = "hello anshika";
+
+    var m1 = document.getElementById('5');
+    m1.innerHTML = "bihsdaiuhcihsi";
+
+    var m1 = document.getElementById('6');
+    m1.innerHTML = "rduhjnkjl";
+
+    var m1 = document.getElementById('7');
+    m1.innerHTML = "hajsljlal";
+
+    var m1 = document.getElementById('8');
+    m1.innerHTML = "dsjojpoks";
+
+})
+var no4 = document.querySelector('#no4');
+no4.addEventListener('click', function() {
+
+    var name = document.getElementById('1');
+    name.innerHTML = "papa";
+    var m1 = document.getElementById('2');
+    m1.innerHTML = "offline";
+
+    var m1 = document.getElementById('3');
+    m1.innerHTML = "hiiii";
+
+    var m1 = document.getElementById('4');
+    m1.innerHTML = "hello anshika";
+
+    var m1 = document.getElementById('5');
+    m1.innerHTML = "bihsdaiuhcihsi";
+
+    var m1 = document.getElementById('6');
+    m1.innerHTML = "rduhjnkjl";
+
+    var m1 = document.getElementById('7');
+    m1.innerHTML = "hajsljlal";
+
+    var m1 = document.getElementById('8');
+    m1.innerHTML = "Thanks!!! 😁😁😁😁";
+
+})
+var no5 = document.querySelector('#no5');
+no5.addEventListener('click', function() {
+
+    var name = document.getElementById('1');
+    name.innerHTML = "saumya";
+    var m1 = document.getElementById('2');
+    m1.innerHTML = "offline";
+
+    var m1 = document.getElementById('3');
+    m1.innerHTML = "hiiii";
+
+    var m1 = document.getElementById('4');
+    m1.innerHTML = "hello anshika";
+
+    var m1 = document.getElementById('5');
+    m1.innerHTML = "bihsdaiuhcihsi";
+
+    var m1 = document.getElementById('6');
+    m1.innerHTML = "rduhjnkjl";
+
+    var m1 = document.getElementById('7');
+    m1.innerHTML = "hajsljlal";
+
+    var m1 = document.getElementById('8');
+    m1.innerHTML = "Thanks!!! 😁😁😁😁";
+})
+var no6 = document.querySelector('#no6');
+no6.addEventListener('click', function() {
+
+    var name = document.getElementById('1');
+    name.innerHTML = "rishabh";
+    var m1 = document.getElementById('2');
+    m1.innerHTML = "offline";
+
+    var m1 = document.getElementById('3');
+    m1.innerHTML = "hiiii";
+
+    var m1 = document.getElementById('4');
+    m1.innerHTML = "hello anshika";
+
+    var m1 = document.getElementById('5');
+    m1.innerHTML = "bihsdaiuhcihsi";
+
+    var m1 = document.getElementById('6');
+    m1.innerHTML = "rduhjnkjl";
+
+    var m1 = document.getElementById('7');
+    m1.innerHTML = "hajsljlal";
+
+    var m1 = document.getElementById('8');
+    m1.innerHTML = "Thanks!!! 😁😁😁😁";
+
+})
+var no7 = document.querySelector('#no7');
+no7.addEventListener('click', function() {
+
+    var name = document.getElementById('1');
+    name.innerHTML = "nilesh";
+    var m1 = document.getElementById('2');
+    m1.innerHTML = "offline";
+
+    var m1 = document.getElementById('3');
+    m1.innerHTML = "hiiii";
+
+    var m1 = document.getElementById('4');
+    m1.innerHTML = "hello anshika";
+
+    var m1 = document.getElementById('5');
+    m1.innerHTML = "bihsdaiuhcihsi";
+
+    var m1 = document.getElementById('6');
+    m1.innerHTML = "rduhjnkjl";
+
+    var m1 = document.getElementById('7');
+    m1.innerHTML = "hajsljlal";
+
+    var m1 = document.getElementById('8');
+    m1.innerHTML = "Are you there??? 🤔🤔🤔";
+
+})
